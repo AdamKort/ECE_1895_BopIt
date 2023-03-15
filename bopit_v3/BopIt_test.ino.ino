facb33168ecf761bdbc27ac15f9dbfac18a6b7fa
Kaleb Foster <64374418+ksf0664@users.noreply.github.com>
2023-03-15 13:06:28 -0400

    Edited v3
    Implemented inputTime variable so user has a limited amount of time before all inputs are read. inputTime decrements each time score increases by 10.
    Not sure that we need to do interrupts, but this needs to be tested. I think we might be fine without them
    Added conditional case where if any incorrect input is sensed that the user is told their final score and sent to an infinite while loop -- need to decide how we want to reset and start the game

---
+++ bopit_v3/BopIt_test.ino.ino
@@ -0,0 +1,137 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <time.h>
+
+const int spinIn = 6;
+const int spinOut = 9;
+const int betIn = 2;
+const int betOut = 10;
+const int spinIndicate  = 8; //Indicate lights act like the 
+const int betIndicate = 7;
+
+int spinInState = 0;
+int betInState = 0;
+int score = 0;
+int inputTime = 300;
+
+bool pass_level = false;
+int randNum = 0;
+
+//I2C pins declaration
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+void setup() {
+  pinMode(betIn, INPUT);   // Button input for the bet it feature
+  pinMode(spinIn, INPUT);  // Button input for the spin it feature
+  pinMode(betOut, OUTPUT);    // Lights up the LED for bet it
+  pinMode(spinOut, OUTPUT);    // Lights up the LED for spin it
+  pinMode(spinIndicate, OUTPUT); //Indicator for which operation to perform
+  pinMode(betIndicate, OUTPUT);  //Indicator for which operation to perform
+  Serial.begin(9600);
+
+  lcd.begin(16,2);//Defining 16 columns and 2 rows of lcd display
+  lcd.backlight();//To Power ON the back light
+
+  // I think this is the right syntax for the interrupts? TBD
+  // attachInterrupt(digitalPinInterrupt(PIN), ISR, mode)
+}
+
+//BEGINNING MAIN LOOP
+void loop() {
+  randNum = random(2);
+
+  // every 10 successful attempt, decrement time to input by 25ms
+  if (score%10 == 0)
+    inputTime -= 25; 
+
+  delay(1000);
+  Serial.print(randNum);
+  //The spin it function
+  if (randNum == 0) {
+    lcd.setCursor(0,0); //Defining positon to write from first row,first column .
+    lcd.print(" Spin it! "); //You can write 16 Characters per line .
+    digitalWrite (spinIndicate, HIGH);
+
+    delay(inputTime);
+    spinInState = digitalRead(spinIn);
+    betInState = digitalRead(betIn);
+    // add other two later
+    if(betInState == HIGH) // (betInState == HIGH || loseInState == HIGH || winInState == HIGH)
+      pass_level = false;
+
+    if (spinInState == HIGH){
+      digitalWrite(spinOut, HIGH); // sets the digital pin 9 on
+      delay(100);  
+      digitalWrite(spinOut, LOW);  // sets the digital pin 9 off
+      pass_level = true;
+    }
+    
+
+    if (pass_level == false){
+      lcd.setCursor(0,0); //Defining positon to write from first row,first column .
+      lcd.print("MISINPUT!!")
+      lcd.print("Final Score: "); //You can write 16 Characters per line .
+      lcd.print(score);
+      score = 0;
+    }
+    else{
+      score++;
+      lcd.setCursor(1,0); //Defining positon to write from first row,first column .
+      lcd.print("Score: "); //You can write 16 Characters per line .
+      lcd.print(score);
+    }
+
+  
+    delay(1000);
+    lcd.clear();
+    digitalWrite (spinIndicate, LOW);
+  }
+  
+  //The bet it function
+  if (randNum == 0) {
+    lcd.setCursor(0,0); //Defining positon to write from first row,first column .
+    lcd.print(" Spin it! "); //You can write 16 Characters per line .
+    digitalWrite (spinIndicate, HIGH);
+
+    delay(inputTime);
+    spinInState = digitalRead(spinIn);
+    betInState = digitalRead(betIn);
+    // add other two later
+    if(betInState == HIGH) // (betInState == HIGH || loseInState == HIGH || winInState == HIGH)
+      pass_level = false;
+
+    if (spinInState == HIGH){
+      digitalWrite(spinOut, HIGH); // sets the digital pin 9 on
+      delay(100);  
+      digitalWrite(spinOut, LOW);  // sets the digital pin 9 off
+      pass_level = true;
+    }
+    
+
+    if (pass_level == false){
+      lcd.setCursor(0,0); //Defining positon to write from first row,first column .
+      lcd.print("MISINPUT!!")
+      lcd.print("Final Score: "); //You can write 16 Characters per line .
+      lcd.print(score);
+      score = 0;
+    }
+    else{
+      score++;
+      lcd.setCursor(1,0); //Defining positon to write from first row,first column .
+      lcd.print("Score: "); //You can write 16 Characters per line .
+      lcd.print(score);
+    }
+  
+    delay(1000);
+    lcd.clear();
+    digitalWrite (betIndicate, LOW);
+  }
+
+  while(score == 0){
+    delay(300);
+    // fail contition met at some point
+    // need to reset the game
+    // could do this via an input?
+  }
+
+
+}
